d165fbf2fa7c1280f120ce5d086b80ed6bb6b44b
EPolishchuk <eldar.polishchuk@gmail.com>
2016-02-22 23:56:14 +0200

    Project 04 - Adding FaceBook API SDK

--- 04-facebook-app/app/app.js
+++ 04-facebook-app/app/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('ngSocial', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngSocial.facebook'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/facebook'});
 }]);
---
+++ 04-facebook-app/app/facebook/facebook.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
+}])
+
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('1669877126605665');
+  $facebookProvider.setPermissions('email', 'public_profile', 'user_posts', 'publish_actions', 'user_photos');
+})
+
+.run(function($rootScope){
+	(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+})
+
+.controller('FacebookCtrl', [function() {
+
+}]);
