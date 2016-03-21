77af1036c8ff597cd6b77882540fb2e83f7a07ae
EPolishchuk <eldar.polishchuk@gmail.com>
2016-03-21 23:17:28 +0200

    Project 6 - creating angular routes and view files

---
+++ 06-knowledge-base/public/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('kB', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/categories', {
+			templateUrl: 'views/categories.view.html',
+			controller: 'CategoriesCtrl'
+		}).
+		when('/articles', {
+			templateUrl: 'views/articles.view.html',
+			controller: 'ArticlesCtrl'
+		}).
+		when('/articles/detail/:id', {
+			templateUrl: 'views/article_details.view.html',
+			controller: 'ArticleDetailsCtrl'
+		}).
+		when('/articles/category/:category', {
+			templateUrl: 'views/cat_articles.view.html',
+			controller: 'ArticlesCategoryCtrl'
+		}).
+		when('/articles/add', {
+			templateUrl: 'views/add_article.view.html',
+			controller: 'ArticlesCreateCtrl'
+		}).
+		when('/articles/edit/:id', {
+			templateUrl: 'views/edit_article.view.html',
+			controller: 'ArticlesEditCtrl'
+		}).
+		otherwise({
+			redirectTo: '/categories'
+		})
+}]);
