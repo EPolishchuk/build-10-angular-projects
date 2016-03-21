61d3c5272e4ecc6c0caa373fba42acfe766c0462
EPolishchuk <eldar.polishchuk@gmail.com>
2016-03-22 00:37:17 +0200

    Project 6 - pages and controllers for get requests

--- 06-knowledge-base/public/controllers/articles.controllers.js
+++ 06-knowledge-base/public/controllers/articles.controllers.js
@@ -4,4 +4,18 @@ angular.module("kB")
 	$http.get('/articles').success(function(data){
 		$scope.articles = data;
 	});
+}])
+
+.controller('ArticlesCategoryCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('/articles/category/' + $routeParams.category).success(function(data){
+		$scope.cat_articles = data;
+		$scope.category = $routeParams.category;
+	});
+}])
+
+.controller('ArticleDetailsCtrl', ['$scope','$http', '$routeParams', function($scope, $http, $routeParams){
+	$http.get('/articles/' + $routeParams.id).success(function(data){
+		$scope.article = data;
+	});
 }]);
+
