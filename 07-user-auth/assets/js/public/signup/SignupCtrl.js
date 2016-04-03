6c3636d157279bae7b1ac665f7e42aa67f1c1162
EPolishchuk <eldar.polishchuk@gmail.com>
2016-04-03 21:53:01 +0300

    Project 07 - setup frontend

---
+++ 07-user-auth/assets/js/public/signup/SignupCtrl.js
@@ -0,0 +1,20 @@
+angular.module('SignupMod').controller('SignupCtrl',['$scope', '$http', function($scope, $http){
+  console.log('Signup Controller Init');
+
+  $scope.runSignup = function() {
+  	console.log('Signing Up ' + $scope.name);
+
+  	//Submit to Sails Server
+  	$http.post('/signup', {
+  		name: $scope.name,
+  		email: $scope.email,
+  		password: $scope.password
+  	})
+  	.then(function onSuccess(response){
+  		window.location = '/user';
+  	})
+  	.catch(function onError(err){
+  		console.log('Error: ' + err);
+  	});
+  }
+}])
--- 07-user-auth/tasks/pipeline.js
+++ 07-user-auth/tasks/pipeline.js
@@ -29,6 +29,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  'js/dependencies/jquery-1.12.2.min.js',  
   'js/dependencies/**/*.js',
   'js/public/signup/SignupMod.js',
   'js/public/signup/SignupCtrl.js',
